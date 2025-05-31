95452ab3a09ea1ef94f0e539e5fb20e828a7492c
Shivedra123 <shivendrasaket2580@gmail.com>
2025-05-31 17:36:49 +0530

    first commit

---
+++ day 01/script.js
@@ -0,0 +1,45 @@
+// one shot one day javascript
+
+
+
+// const mytime = new Date()
+// console.log(mytime.getDate())
+
+
+
+  
+
+// const person = {
+//     name: "shivendra mauhariya" ,
+//     Age: 16,
+//     address: "mauhariya" ,
+//     male: true
+
+
+
+// }
+// console.log(person["Age"])
+
+
+// function mymarks (...all) {
+//     return all
+// }
+// console.log(mymarks(90, 91, 85, 95, 84))
+
+
+
+// const newarry = [91, 50, 80, 89]
+// function returnvalue(getarry){
+//     return getarry[3]
+// }
+// console.log(returnvalue(newarry))
+
+
+
+
+
+
+
+
+
+
