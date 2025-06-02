19e27ce574ea9fad473c3f0beb4600e7871d4f4b
Shivedra123 <shivendrasaket2580@gmail.com>
2025-06-02 15:16:46 +0530

    js concept

--- day 01/types_of_scope_02.js
+++ day 01/types_of_scope_02.js
@@ -10,14 +10,39 @@
 // tesing()
 
 
-// Local scope 
+// if (true) {
+//     let  a = 84;
+//     const b = 85;
+//     console.log("inner: ",a ,b);
+// } 
 
-function test() {[
-    var b = 85;
-    console.log(b);
+
+
+// scope level and hosting
+
+// if (true) {
+//     const username = "shivendra mauhariya"
+//     if (username === "shivendra mauhariya") {
+//         const platform = " instagram"
+//         console.log(username + platform);
+
+//     } 
+
+// } 
+// console.log(platform);
+
+
+
+// function add(num) {
+//     return num * 2
+// }
+// console.log(add(5))
+
+if (0.1+ 0.2 == 0.3) {
+    console.log(true);
+} else  if (0.1 + 0.2 != 0.3) {
+    console.log(false);
 }
 
-
-
-
+console.log(2+"2")
 
