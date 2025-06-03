de3e72e865ccd599990f2c72846412acb4bbb995
Shivedra123 <shivendrasaket2580@gmail.com>
2025-06-03 18:24:37 +0530

    clear

---
+++ day 02/script.js
@@ -0,0 +1,11 @@
+const inputName = prompt("shivendra mauhariya");
+
+let username, password;
+
+if (inputName === "shivendra mauhariya") {
+  username = "Shivendra_08";
+  password = "Shivendra@2024";
+  alert(`Username: ${username}\nPassword: ${password}`);
+} else {
+  alert('Wrong details');
+}   
---
+++ day 03/script.js
@@ -0,0 +1,18 @@
+// const addnum = (paisa1, paisa2) => {
+//     return paisa1 * paisa2
+// }
+// console.log(addnum(8, 4))
+
+// const addnum = (paisa1, paisa2) => (paisa1 + paisa2)
+// console.log(addnum(80, 4));
+
+
+
+// IIfe in js 
+
+ 
+(function proplyer () {
+    console.log(`Shiivendra`);
+}) ()
+
+q
