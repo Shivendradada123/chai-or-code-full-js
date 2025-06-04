1be64c2f6692f725f33d3e4c9d8c6b8ab10f2c2d
Shivedra123 <shivendrasaket2580@gmail.com>
2025-06-04 14:55:30 +0530

    control flow

---
+++ 04_control_flow/script.js
@@ -0,0 +1,17 @@
+//  const myresult =    prompt()
+
+// if(myresult > 80) {
+//     alert("yes you are codder ")
+// } else{
+//     alert("you are backchod")
+// }
+
+
+const bankblance = 1299
+
+if(bankblance > 800) {
+    console.log('yes you are rech')
+}else{
+    alert('chal garib nikal')
+}
+
