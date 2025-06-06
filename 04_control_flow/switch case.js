80c35d9305be8a3684dab0b11ca283f8e255993e
Shivedra123 <shivendrasaket2580@gmail.com>
2025-06-06 15:17:27 +0530

    loops

---
+++ 04_control_flow/switch case.js
@@ -0,0 +1,20 @@
+
+const day = 6
+
+switch (day) {
+    case 1:
+        console.log("friday")
+        
+        break;
+    case 5:
+        console.log("wednesday")
+        
+        break;
+    case 6:
+        console.log("sunday")
+        
+        break;
+
+    default:
+        break;
+}
---
+++ 05_loops_/loop.js
@@ -0,0 +1,9 @@
+// For loops 
+
+const name = "Shivendra mauhariya";
+
+for (let i = 0; i < name.length; i++) {
+    const letter = name[i];
+    console.log(letter);
+    
+}
