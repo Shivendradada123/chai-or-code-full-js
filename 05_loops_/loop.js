8307a3eebb47e077856a6a8d3b3319db8e643bdf
Shivedra123 <shivendrasaket2580@gmail.com>
2025-06-07 20:00:35 +0530

    loops

--- 05_loops_/loop.js
+++ 05_loops_/loop.js
@@ -1,9 +1,36 @@
 // For loops 
 
-const name = "Shivendra mauhariya";
+// const name = "Shivendra mauhariya";
 
-for (let i = 0; i < name.length; i++) {
-    const letter = name[i];
-    console.log(letter);
+// for (let i = 0; i < name.length; i++) {
+//     const letter = name[i];
+//     console.log(letter);
+    
+// }
+
+// const mynewarry = ["shivendra", "adidtya", "akash"]
+// for (let index = 0; index < mynewarry.length; index++) {
+//     const element = mynewarry[index];
+//     console.log(element);
+    
+// }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+const Obj = {
+    Name: "shivendra",
+    age: 16,
+    village: "mauhariya"
+}
+for (const key in Obj) {
+    
+        const element = Obj[key];
+        console.log( key, element)
+        
     
 }
+
