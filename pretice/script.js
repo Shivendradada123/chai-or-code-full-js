7ee6db97223ece65d6eeed1a984fb6571b66eb52
Shivedra123 <shivendrasaket2580@gmail.com>
2025-06-11 14:38:02 +0530

    +

---
+++ pretice/script.js
@@ -0,0 +1,30 @@
+
+//    chapter 01
+                                                                                                                                               
+                                         
+//    if (true) {
+        
+//     let a = "kismat";
+// const b = "kharab";
+// var age = 16;                            
+    
+// }
+// console.log(a)
+// console.log(b)
+// console.log(age)
+
+
+
+
+
+//  chapter 02
+
+
+// Primitive
+
+
+let a = "shivendra"
+let b = a;
+
+    console.log(b)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+
