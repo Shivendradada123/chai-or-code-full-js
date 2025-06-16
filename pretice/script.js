ecb339e326f400ecc6553b67c1722ba63ae51de8
Shivedra123 <shivendrasaket2580@gmail.com>
2025-06-16 17:07:37 +0530

    event end+

--- pretice/script.js
+++ pretice/script.js
@@ -1,30 +1,42 @@
 
-//    chapter 01
+// //    chapter 01
                                                                                                                                                
                                          
-//    if (true) {
+// //    if (true) {
         
-//     let a = "kismat";
-// const b = "kharab";
-// var age = 16;                            
+// //     let a = "kismat";
+// // const b = "kharab";
+// // var age = 16;                            
     
-// }
-// console.log(a)
-// console.log(b)
-// console.log(age)
+// // }
+// // console.log(a)
+// // console.log(b)
+// // console.log(age)
 
 
 
 
 
-//  chapter 02
+// //  chapter 02
 
 
-// Primitive
+// // Primitive
 
 
-let a = "shivendra"
-let b = a;
+// let a = "shivendra"
+// let b = a;
 
-    console.log(b)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+//     console.log(b)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+
+
+function startTest() {
+  // Dummy values generate karenge
+  const download = (Math.random() * 100 + 10).toFixed(2); // 10–110 Mbps
+  const upload = (Math.random() * 50 + 5).toFixed(2); // 5–55 Mbps
+  const ping = Math.floor(Math.random() * 100 + 10); // 10–110 ms
+
+  document.getElementById("download").innerText = download;
+  document.getElementById("upload").innerText = upload;
+  document.getElementById("ping").innerText = ping;
+}
 
